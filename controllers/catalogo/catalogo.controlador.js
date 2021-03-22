76c07668905893fc81f042536fdc169fb3260d65
Alexis Dorado <alexisdorado@unicauca.edu.co>
2021-03-22 14:20:51 -0500

    Tarea 384 | Corre

--- controllers/catalogo/catalogo.controlador.js
+++ controllers/catalogo/catalogo.controlador.js
@@ -249,7 +249,7 @@ try {
 };
 
 const mostrarCatalogo = async (req, res) => {
-  const catalogoCompleto = await Catalogo.find();
+  const catalogoCompleto = await Estampillas.find();
 
   res.json({
     ok: true,
